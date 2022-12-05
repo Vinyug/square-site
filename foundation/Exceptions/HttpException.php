8fd7551daccfa360ea8425f1310d621fb599fc43
Vinyug <vincentguyomarch89@gmail.com>
2022-12-05 14:53:52 +0100

    composer illuminate v, add default.html, add view HttpException

--- foundation/Exceptions/HttpException.php
+++ foundation/Exceptions/HttpException.php
@@ -2,13 +2,18 @@
 
 namespace VGuyomarch\Foundation\Exceptions;
 
+use VGuyomarch\Foundation\View;
+
 class HttpException extends \Exception 
 {
     // afficher une erreur HTTP destiné au client
     public static function render(int $httpCode = 404, string $message = 'Page non trouvé'): void
     {
         http_response_code($httpCode);
-        echo "<h1> Erreur $httpCode : $message </h1>";
+        View::render('errors.default', [
+            'httpCode' => $httpCode,
+            'message' => $message
+        ]);
         die;
     }
 }
