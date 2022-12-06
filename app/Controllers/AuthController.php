9bfb7e6486097b6a7ecf8d706e76e3b2dd52ba9f
Vinyug <vincentguyomarch89@gmail.com>
2022-12-06 23:27:03 +0100

    Authenticate redirection

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,12 +3,18 @@
 namespace App\Controllers;
 
 use VGuyomarch\Foundation\AbstractController;
+use VGuyomarch\Foundation\Authentication;
 use VGuyomarch\Foundation\View;
 
 class AuthController extends AbstractController
 {
     public function registerForm(): void
     {
+        // si user authenticate redirection vers home
+        if(Authentication::check()) {
+            $this->redirection('home');
+        }
+
         View::render('auth.register');
     }
 }
