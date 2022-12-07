ab86f8016a34ed417ba941bc8e4815b7e2ddd946
Vinyug <vincentguyomarch89@gmail.com>
2022-12-07 12:55:49 +0100

    add view home(compte)

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,12 +3,24 @@
 namespace App\Controllers;
 
 use VGuyomarch\Foundation\AbstractController;
-
+use VGuyomarch\Foundation\Authentication as Auth;
+use VGuyomarch\Foundation\View;
 
 class HomeController extends AbstractController
 {
     public function index(): void
     {
-        
+        // si user non authenticate, redirect login form
+        if(!Auth::check()) {
+            $this->redirection('login.form');
+        }
+
+        // récupérer les datas de User pour compléter la view home (compte)
+        $user = Auth::get();
+
+        // View home
+        View::render('home', [
+            'user' => $user,
+        ]);
     }
 }
