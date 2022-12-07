cdcbe20ce1b3f05fb6eb43a55ed08e8c5eef5f65
Vinyug <vincentguyomarch89@gmail.com>
2022-12-07 10:45:55 +0100

    part 2 - AuthController get fields/validator, Validator rule, register.html twig

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use VGuyomarch\Foundation\AbstractController;
 use VGuyomarch\Foundation\Authentication as Auth;
+use VGuyomarch\Foundation\Session;
+use VGuyomarch\Foundation\Validator;
 use VGuyomarch\Foundation\View;
 
 class AuthController extends AbstractController
@@ -23,6 +25,24 @@ class AuthController extends AbstractController
     {
         if(Auth::check()) {
             $this->redirection('home');
+        }
+
+        // récupération des données de form pour validation
+        $validator = Validator::get($_POST);
+        // indiquer toutes les règles à respecter par field
+        $validator->mapFieldsRules([
+            'name' => ['required', ['lengthMin', 5]],
+            'email' => ['required', 'email', ['unique', 'email', 'users']],
+            'password' => ['required', ['lengthMin', 8], ['equals', 'password_confirmation']],
+        ]);
+        
+        // action si fields non renseigné correctement
+        if(!$validator->validate()) {
+            // récupère le message d'erreur
+            Session::addFlash(Session::ERRORS, array_column($validator->errors, 0));
+            // field reprend une valeur déjà renseigné
+            Session::addFlash(Session::OLD, $_POST);
+            $this->redirection('register.form');
         }
     }
 }
--- foundation/Validator.php
+++ foundation/Validator.php
@@ -2,6 +2,7 @@
 
 namespace VGuyomarch\Foundation;
 
+use Illuminate\Database\Capsule\Manager as Capsule;
 use Valitron\Validator as ValitronValidator;
 
 // Utilisation de valitron pour simplifier la validation de données (formulaire)
@@ -20,6 +21,11 @@ class Validator
 
     protected static function addCustomRules(ValitronValidator $validator): void
     {
-        // Custom rules here
+        // règle valeur unique d'un champ
+        $validator->addRule('unique', function (string $field, mixed $value, array $params, array $fields) {
+            // requete return un bool pour vérifier si value exist dans BDD
+            return !Capsule::table($params[1])->where($params[0], $value)->exists();
+        }, '{field} est invalide');
+
     }
 }
