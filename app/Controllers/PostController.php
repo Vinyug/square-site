de3dec89745f09af8acc6ebd787bce11d2beada6
Vinyug <vincentguyomarch89@gmail.com>
2022-12-08 11:50:24 +0100

    part 1 : create post PostController, Post, Validator, create.html

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Models\Post;
+use Cocur\Slugify\Slugify;
 use VGuyomarch\Foundation\AbstractController;
 use VGuyomarch\Foundation\Authentication as Auth;
+use VGuyomarch\Foundation\Session;
+use VGuyomarch\Foundation\Validator;
 use VGuyomarch\Foundation\View;
 
 class PostController extends AbstractController
 {
+    // afficher view
     public function create(): void
     {
         if(!Auth::checkIsAdmin()) {
@@ -16,4 +21,67 @@ class PostController extends AbstractController
 
         View::render('posts.create');
     }
+
+    // enregistrer un nouveau post
+    public function store(): void
+    {
+        if(!Auth::checkIsAdmin()) {
+            $this->redirection('login.form');
+        }
+
+        // règle Validator
+        // le + permet de fusionner les arrays des superglobals
+        $validator = Validator::get($_POST + $_FILES);
+        $validator->mapFieldsRules([
+            'title' => ['required', ['lengthMin', 3]],
+            'post' => ['required', ['lengthMin', 3]],
+            'file' => ['required_file', 'image', 'square'],
+            'body' => ['required', ['lengthMin', 3]],
+        ]);
+
+        // action si invalide
+        if(!$validator->validate()) {
+            Session::addFlash(Session::ERRORS, $validator->errors());
+            Session::addFlash(Session::OLD, $_POST);
+            $this->redirection('posts.create');
+        }
+
+        // Action si validé
+        $slug = $this->slugify($_POST['title']);
+        // récupère l'extension de l'image
+        $ext = pathinfo(
+            $_FILES['file']['name'],
+            PATHINFO_EXTENSION
+        );
+        // utilisation du slug pour renommer l'image
+        $filename = sprintf('%s.%s', $slug, $ext);
+
+        // enregistrer le fichier en BDD
+        if(!move_uploaded_file(
+            $_FILES['file']['tmp_name'],
+            sprintf('%s/public/img/%s', ROOT, $filename)
+        )) {
+            Session::addFlash(Session::ERRORS, ['file' => [
+                'Il y a eu un problème lors de l\'envoi. Retentez votre chance !',
+            ]]);
+            Session::addFlash(Session::OLD, $_POST);
+            $this->redirection('posts.create');
+            
+        }
+    }
+
+    // creation slug article avec cocur/slugify
+    public function slugify(string $title): string 
+    {
+        $slugify = new Slugify();
+        $slug = $slugify->slugify($title);
+        // permettre d'avoir slug unique, si plusieurs titre en BDD sont identiques
+        $i = 1;
+        $unique_slug = $slug;
+        while(Post::where('slug', $unique_slug)->exists()) {
+            $unique_slug = sprintf('%s-%s', $slug, $i++);
+        }
+        return $unique_slug;
+    }
+
 }
--- foundation/Validator.php
+++ foundation/Validator.php
@@ -33,6 +33,27 @@ class Validator
             $user = Authentication::get();
             return password_verify($value, $user->password);
         }, '{field} est erroné');
+        
+        // règle pour vérifier si le fichier est présent et s'il n'y a pas eu d'erreur d'upload
+        $validator->addRule('required_file', function (string $field, mixed $value, array $params, array $fields) {
+            return isset($_FILES[$field]) && $_FILES[$field]['error'] === UPLOAD_ERR_OK;
+        }, '{field} est obligatoire');
+        
+        // règle pour vérifier qu'il s'agit d'une image (à partir du type mime)
+        $validator->addRule('image', function (string $field, mixed $value, array $params, array $fields) {
+            if(isset($_FILES[$field]) && $_FILES[$field]['error'] === UPLOAD_ERR_OK) {
+                return str_starts_with($_FILES[$field]['type'], 'image/');
+            }
+            return false;
+        }, '{field} doit être une image');
 
+        // règle vérifier si image est carré
+        $validator->addRule('square', function (string $field, mixed $value, array $params, array $fields) {
+            if(isset($_FILES[$field]) && $_FILES[$field]['error'] === UPLOAD_ERR_OK) {
+                [$width, $height] = getimagesize($_FILES[$field]['tmp_name']);
+                return $width === $height;
+            }
+            return false;
+        }, '{field} doit être carré (largeur = hauteur)');
     }
 }
